5bd1d6c29563269437656805ae3960ca8ee614d5
CaioHAlves <caio.henriquealves@outlook.com>
2025-01-04 19:53:17 -0300

    atualização no patch do grupo

--- src/controllers/groups/patchGroups.ts
+++ src/controllers/groups/patchGroups.ts
@@ -3,7 +3,6 @@ import { Groups } from '../../models/groups'
 import { IGroups } from '../interfaces/groups'
 import { Response } from '../interfaces/response'
 import { Stores } from '../../models/stores'
-import { ObjectId } from 'mongodb'
 
 @Route('groups')
 @Tags("Groups")
@@ -21,47 +20,82 @@ export class PatchGroups extends Controller {
       }
     }
 
-    return Groups.findByIdAndUpdate({ _id: id }, {
-      $addToSet: {
-        lojasVinculadas: { $each: body.lojasVinculadas },
-        codGrupo: body.codGrupo,
-        nomeGrupo: body.nomeGrupo,
-        responsavel: body.responsavel
-      },
-    }, { new: true })
-      .then(async groupUpdated => {
-
-        if (body.lojasVinculadas !== undefined) {
-          const storesUpdated = body.lojasVinculadas?.map((group) => ({
-            updateOne: {
-              filter: { _id: new ObjectId(group.id) },
-              update: {
-                $addToSet: {
-                  gruposVinculados: {
-                    id: groupUpdated!._id.toString(),
-                    codGrupo: groupUpdated!.codGrupo,
-                    nomeGrupo: groupUpdated!.nomeGrupo
-                  }
+    try {
+      const group = await Groups.findById({ _id: id })
+  
+      if (!group) {
+        return {
+          code: 404,
+          message: 'Grupo não encontrado'
+        }
+      }
+  
+      if (body.lojasVinculadas) {
+        // Obter os IDs das lojas antes da atualização
+        const previousStoreIds = group.lojasVinculadas.map(store => store.id)
+      
+        // Atualizar as lojas vinculadas no grupo
+        group.lojasVinculadas = body.lojasVinculadas.length
+          ? group.lojasVinculadas.concat(body.lojasVinculadas).reduce((acc, current) => {
+              if (!acc.some(item => item.id === current.id)) {
+                acc.push(current)
+              }
+              return acc
+            }, [] as typeof group.lojasVinculadas)
+          : []
+      
+        // Obter os IDs das lojas após a atualização
+        const currentStoreIds = group.lojasVinculadas.map(store => store.id)
+      
+        // Identificar as lojas removidas
+        const removedStoreIds = previousStoreIds.filter(id => !currentStoreIds.includes(id))
+      
+        // Atualizar as lojas adicionando o grupo
+        if (body.lojasVinculadas.length) {
+          await Stores.updateMany(
+            { _id: { $in: body.lojasVinculadas.map(store => store.id) } },
+            {
+              $addToSet: {
+                gruposVinculados: {
+                  id: group.id,
+                  codGrupo: group.codGrupo,
+                  nomeGrupo: group.nomeGrupo,
                 }
               }
             }
-          }))
+          )
+        }
+      
+        // Remover o grupo das lojas que foram desvinculadas
+        if (removedStoreIds.length || !body.lojasVinculadas.length) {
+          await Stores.updateMany(
+            { _id: { $in: removedStoreIds }},
+            {
+              $pull: {
+                gruposVinculados: { id: group.id },
+              }
+            }
+          )
+        }
+      }
 
-          await Stores.bulkWrite(storesUpdated)
-        }
+      group.codGrupo = body.codGrupo || group.codGrupo
+      group.nomeGrupo = body.nomeGrupo || group.nomeGrupo
+      group.responsavel = body.responsavel || group.responsavel
 
-        return {
-          code: 200,
-          message: 'Grupo alterado com sucesso',
-          group: body
-        }
-      })
-      .catch((err) => {
-        return {
-          code: 500,
-          error: err,
-          message: 'Erro ao alterar grupo. Conexão com o servidor não estabelecida'
-        }
-      })
+      await group.save()
+  
+      return {
+        code: 200,
+        message: 'Grupo alterado com sucesso',
+        data: group
+      }
+    } catch (error) {
+      return {
+        code: 500,
+        message: 'Erro interno no servidor',
+        error
+      }
+    }
   }
 }
